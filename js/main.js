570c7e0064baeee9a43883b0db52beb506967ffb
EngBola <bolakamalpc@gmail.com>
2022-10-24 12:48:07 +0200

    display search

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-
 var app = {
     place: 65,
     NameOfCountry: "Egypy",
     Iso3Name: "EG",
     CapitalCity: "Cairo",
+    curentSelect:"cairo",
     days:4,
+    today: new Date,
 }
+
+const months = ["January","February","March","April","May","Jun","July","August","September","October","November","December"];
+const dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 AllCountries();
 function displayData() {
     countryName.innerHTML = app.NameOfCountry;
@@ -13,22 +17,14 @@ function displayData() {
     im[0].src = `https://www.countryflagicons.com/FLAT/64/${app.Iso3Name.toUpperCase()}.png`;
     im[1].src = `https://www.countryflagicons.com/FLAT/64/${app.Iso3Name.toUpperCase()}.png`;
     getCity();
-    model();
-    
+    model();    
 }
+
 let dropMenu = document.querySelector(".dropdown-menu");
 let countryName = document.querySelector(".region h5");
 let im = document.querySelectorAll(".flag");
 let citesMenu = document.querySelector(".myCites ul");
 displayData();
-
-
-
-
-function setCites() {
-
-}
-
 
 async function AllCountries() {
     let countries = await fetch("./countries.json");
@@ -36,7 +32,7 @@ async function AllCountries() {
         countries = await countries.json();
         let droplist = "";
         for (let i = 0; i < countries.length; i++) {
-            droplist += `<li><a class="dropdown-item" href="#" data-target="${i}" iso2-Name="${countries[i].iso2}">${countries[i].name}</a></li>`;
+            droplist += `<li><a class="dropdown-item" href="#weather-state" data-target="${i}" iso2-Name="${countries[i].iso2}">${countries[i].name}</a></li>`;
         }
         dropMenu.innerHTML = droplist;
         let dropItems = document.querySelectorAll(".dropdown-item");
@@ -47,46 +43,30 @@ async function AllCountries() {
                 app.Iso3Name = dropItems[i].getAttribute("iso2-Name");
                 app.CapitalCity = countries[app.place].capital;
                 displayData();
-                
-                // console.log(countries[app.place])
             });
-            
             
         }
     }
-
 }
-
-// async function getDegree(city=cairo , day=1){
-//     let forcast = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=0905567266db456987580924221110&q=${city}&days=${day}`)
-//     forcast = await forcast.json();
-//     console.log(forcast.current.temp_c);
-//     // return forcast.current.temp_c;
-//     }
-//     getDegree('cairo')
-
-
 async function getCity() {
     let city = await fetch(`./countries.json`)
     if (city.ok && 400 != city.status) {
         city = await city.json();
         let cit = city[app.place].states;
         let listcity = "";
-
         if (cit.length == 0) {
             listcity = app.NameOfCountry;
             let forcast = await fetch(`https://api.weatherapi.com/v1/current.json?key=0905567266db456987580924221110&q=${listcity}&aqi=no`)
                 if (forcast.ok && 400 != forcast.status) {
                     forcast = await forcast.json();
                     let temp = forcast.current.temp_c;
-                    // let curntC = forcast.current.name;
                     listcity += `
-                        <li class="d-flex justify-content-between text-white-50 p-1">
+                        <li class="d-flex justify-content-between text-white-50 p-1" >
+                        <a href="#weather-state" class="text-decoration-none">
                         <h6>${app.NameOfCountry}</h6>
-                        <p>${temp}<sup>o</sup></p>
+                        <p>${temp}<sup>o</sup></p></a>
                         </li>
                         `;
-                    
                 } else {
                     // console.log("dont worry")
                 }
@@ -96,17 +76,16 @@ async function getCity() {
                 if (forcast.ok && 400 != forcast.status) {
                     forcast = await forcast.json();
                     let temp = forcast.current.temp_c;
-                    listcity += `
+                    listcity += `<a href="#weather-state" class="text-decoration-none">
                         <li class="d-flex justify-content-between text-white-50 p-1">
                         <h6>${cit[i].name}</h6>
                         <p>${temp}<sup>o</sup></p>
-                        </li>
+                        </li></a>
                         `;
-                        
-                        
                 } else {
                     continue;
                 }
+                notOk(i!=cit.length-1);
             }
         }
         citesMenu.innerHTML = listcity;
@@ -114,30 +93,31 @@ async function getCity() {
         for(let i=0 ;i<citesList.length;i++){
             citesList[i].addEventListener("click", function(e){
                 let select =citesList[i].querySelector("h6");
-                model(select.innerHTML);
-                // console.log(select);
+                app.curentSelect=select.innerHTML
+                model(app.curentSelect);
             })
         }
-        
     }
-    
-    
-    // console.log(listcity)
-    // console.log(city[app.place].states)
+
 }
 
-// AllCountries();
+
+
+
 async function model(n=app.CapitalCity,d=app.days){
 
     let thisDay= document.querySelector(".main-day");
     let thisDate =document.querySelector(".date");
     let thisDayPram =document.querySelector(`.weahterPram`);
     let more =document.querySelector(".moreInfo");
+    let mordays =document.querySelector(".days");
 
-    let forcast = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=0905567266db456987580924221110&q=${n}&aqi=no&days=${d}`)
+    let forcast = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=0905567266db456987580924221110&q=${n}&aqi=no&days=${d}`)
         if (forcast.ok && 400 != forcast.status) {
             forcast = await forcast.json();
+            // console.log(new Date(forcast.location.localtime_epoch))
             let temp = forcast.current.temp_c;
+            thisDate.innerHTML= forcast.current.last_updated;
             thisDayPram.innerHTML=`
             <h2 class="fw-bolder">${forcast.location.name}</h2>
                                     <p class="main-day-degree">${temp}<sup class="text-warning">o</sup>C</p>
@@ -147,14 +127,96 @@ async function model(n=app.CapitalCity,d=app.days){
                                     </div>
             `;
             more.innerHTML=`
-                                    <span><img src="images/icon-umberella.png" alt=""> ${forcast.current.wind_degree}%</span>
-                                    <span><img src="images/icon-compass.png" alt=""> ${forcast.current.wind_dir}</span>
-                                    <span><img src="images/icon-wind.png" alt=""> ${forcast.current.wind_kph}Km/h</span>
+                                    <span><img src="images/icon-umberella.png" alt="#"> ${forcast.forecast.forecastday[0].day.avghumidity}%</span>
+                                    <span><img src="images/icon-compass.png" alt="#"> ${forcast.current.wind_dir}</span>
+                                    <span><img src="images/icon-wind.png" alt="#"> ${forcast.current.wind_kph}Km/h</span>
             `
-            // console.log(forcast.forecast)
+            let allDays =forcast.forecast.forecastday
+            let collect="";
+            for(let i=1;i<allDays.length;i++){
+                collect +=`
+                        <div class="col-sm-4">
+                            <div class="main-day container mb-2 p-2 ">
+                                <div class=" d-flex flex-wrap justify-content-around align-items-center rounded-top p-2 bg-dark text-white-50">
+                                    <h6>Satarday</h6>
+                                    <h6><span class="bg-light  rounded-2 text-dark">${allDays[i].date}</span> October</h6>
+                                </div>
+                                <div class="  px-2 test-center text-dark">
+                                    <h4 class="">${allDays[i].day.avgtemp_c}<sup class="text-warning">o</sup>C</h4>
+                                    <div>
+                                        <span class="mb-0">${allDays[i].day.condition.text}</span>
+                                        <img src="${allDays[i].day.condition.icon}" alt="#" >
+                                    </div>
+                                </div>
+                                <div class=" d-flex flex-wrap justify-content-around align-items-center rounded-bottom p-1 bg-dark text-white-50 ">
+                                    <span "><i class="fa-solid fa-umbrella text-warning"></i> ${allDays[i].hour[0].humidity}%</span>
+                                    <span "><i class="fa-regular fa-compass text-warning"></i> ${allDays[i].hour[0].wind_dir}</span>
+                                    <span "><i class="fa-solid fa-wind text-warning"></i> ${allDays[i].hour[0].wind_kph}Km/h</span>
+                                </div>
+                            </div>
+                        </div>
+                    `
+            }
+            mordays.innerHTML=collect;
         }
-        
     
 }
 
 
+
+
+
+
+
+let loder = document.querySelector(".cites .loding")
+let notOk =(s)=>{
+    let t 
+    if(s==true){
+        // console.log("ok")
+        loder.classList.replace("d-none","d-block");
+        t = 1000;
+    }else{
+        loder.classList.replace("d-block","d-none");
+    }
+    
+    $(".crcl").fadeIn(t);
+    $(".crcl").fadeOut(t);
+    
+}
+
+let filter = document.querySelectorAll(".filter ul li");
+for(let i=0;i<filter.length;i++){
+    filter[i].addEventListener("click" , function(){ 
+    model(app.curentSelect,this.value)
+    })
+}
+
+
+(function(){
+    let thisDay = dayName[new Date().getUTCDay()];
+    console.log(thisDay)
+    let ti =app.today.getMonth();
+    for(let i=0;i<12;i++){
+    if(ti<12){
+        console.log(months[ti++])
+    }else{
+        ti=0;
+        console.log(months[ti++])
+    }
+    }
+})()
+
+let search= document.querySelector(".find .btn");
+let find = document.querySelector(".find input");
+search.addEventListener("click", function(){
+    
+    model(app.curentSelect = find.value);
+});
+find.addEventListener("keyup" , function(){
+    if(find.value.length > 2){
+        model(app.curentSelect = find.value);
+    }else{
+        model(app.CapitalCity);
+    }
+    
+})
